9a6c7f42ff0e20fcfc6f2f5720a42704a795fc96
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2025-04-03 10:21:03 -0300

    Excel depósitos

--- src/app/components/Depositos/buscar-depositos/buscar-depositos.component.ts
+++ src/app/components/Depositos/buscar-depositos/buscar-depositos.component.ts
@@ -4,10 +4,23 @@ import { buscarDepositos, buscarDepositosPorNroDeposito } from '../../../api/dep
 import { getUnidades } from '../../../api/unidades.service';
 import { TableComponentDeposito } from './table/table.component';
 import { ColumnDef } from '@tanstack/angular-table';
+import { Workbook } from 'exceljs';
+import fs from 'file-saver';
 // Tipo
 type Unidad = {
   id: number,
   unidad: string,
+}
+
+type Deposito = {
+  nroDeposito: number,
+  fecha: Date,
+  periodoArqueo: Date,
+  unidad: string,
+  banco: string,
+  cuenta: string,
+  boleta: number,
+  importe: number,
 }
 
 @Component({
@@ -152,6 +165,83 @@ onKeydown(event: KeyboardEvent) {
       this.mostrarOpciones = false;
     }
   }
+
+  
+  imprimir(){
+      
+      
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('Depósitos');
+  
+    worksheet.mergeCells('A1:B1'); // Unir celdas
+    worksheet.getCell('A1').value = 'JEFATURA DE POLICÍA';
+    // worksheet.getCell('A1').alignment = { horizontal: 'center' };
+    worksheet.getCell('A1').font = { bold: true, size: 10 };
+  
+    worksheet.mergeCells('A2:D2'); // Unir celdas
+    worksheet.getCell('A2').value = 'DIRECCIÓN DE ADMINISTRACIÓN';
+    // worksheet.getCell('A2').alignment = { horizontal: 'center' };
+    worksheet.getCell('A2').font = { bold: true, size: 10 };
+  
+    worksheet.mergeCells('A4:H4'); // Unir celdas
+    worksheet.getCell('A4').value = 'INFORME DE DEPÓSITOS';
+    worksheet.getCell('A4').alignment = { horizontal: 'center' };
+    worksheet.getCell('A4').font = { bold: true, size: 10 };
+  
+    worksheet.mergeCells('F1:H1'); // Unir celdas
+    worksheet.getCell('F1').value = `Fecha de impresión: ${new Date().toLocaleDateString("es-AR")}`;
+  
+    worksheet.mergeCells('A6:G6'); // Unir celdas
+    worksheet.getCell('A6').value = `Periodo desde ${new Date(this.buscarDepositosForm.value.Desde).toLocaleDateString("es-AR")} hasta ${new Date(this.buscarDepositosForm.value.Hasta).toLocaleDateString("es-AR")}`;
+    // worksheet.getCell('A4').alignment = { horizontal: 'center' };
+    worksheet.getCell('A6').font = { bold: true, size: 10 };
+  
+  
+  
+  
+    // Encabezados personalizados en la fila 8
+  worksheet.getRow(8).values = [
+   'Nro Depósito', 'Fecha', 'Periodo Arqueo', 'Unidad', 'Banco', 'Cuenta', 'Boleta', 'Importe'
+  ];
+  
+  // Establecer ancho de columnas manualmente
+  worksheet.getColumn(1).width = 15; // Nro Depósito
+  worksheet.getColumn(2).width = 20; // Fecha
+  worksheet.getColumn(3).width = 20; // Periodo Arqueo
+  worksheet.getColumn(4).width = 30; // Unidad
+  worksheet.getColumn(5).width = 25; // Banco
+  worksheet.getColumn(6).width = 10; // Cuenta
+  worksheet.getColumn(7).width = 10; // Boleta
+  worksheet.getColumn(8).width = 10; // Importe
+  
+  // Inserción de datos comenzando en la fila 9
+  this.depositos.forEach((data: Deposito, index:number) => {
+    const rowIndex = 9 + index; // Empieza en la fila 9
+    worksheet.getRow(rowIndex).values = [
+      data.nroDeposito,
+      data.fecha,
+      data.periodoArqueo,
+      data.unidad,
+      data.banco,
+      data.cuenta,
+      data.boleta,
+      '$' + data.importe
+    ];
+  });
+  
+  
+        // Agregar los datos
+     //   worksheet.insertRows(9, transformedData);
+    
+        // Crear archivo Excel
+        workbook.xlsx.writeBuffer().then((buffer) => {
+          const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+          fs.saveAs(blob, 'Depositos.xlsx');
+        });
+  
+  
+      // listaEntregas viene con un NroEntrega y 
+    }
 
 
 }
